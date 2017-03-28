b8431cdf820ea3516ecb0fe5f974ecba6047be77
JHanbury <JHanbury@storm.ie>
2017-03-28 11:09:33 +0100

    added the demo page

---
+++ MIDemoForms/MIDemoForms/MIDemoForms/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MIDemoForms
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Image.Source = ImageSource.FromUri(new Uri("https://www.marine.ie/Home/sites/default/files/MIFiles/Images/General/Marine_logo.jpg"));
+        }
+    }
+}
